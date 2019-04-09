008a21b9d7b992f26a7d1451e244feaf7336a690
Joe Chadwick <joe.chadwick@gamesys.co.uk>
2019-04-09 14:29:50 +0100

    Snake is working and fills dead ends

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/move', (request, response) => {
   //   move: 'left', // one of: ['up','down','left','right']
   // }
   //console.log(request.body)
-  data = move(request.body.board)
+  data = move(request.body) // move(request.body)
 
   return response.json(data)
 })
